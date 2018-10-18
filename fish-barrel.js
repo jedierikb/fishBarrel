edc6bcee673a09747ac4a9d673f380a663997906
Erik Blankinship <erikb@mediamods.com>
2018-10-17 21:20:36 -0400

    testing how npm releases work

--- fish-barrel.js
+++ fish-barrel.js
@@ -1,1 +1,7 @@
+/*jshint esversion: 6 */
 "use strict";
+
+module.exports = function fishBarrel( string ) {
+  if (typeof string !== "string") throw new TypeError("fish barrel wants a string!");
+  return string.replace(/\s/g, "");
+};
